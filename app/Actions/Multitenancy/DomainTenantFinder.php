c08372e50fcea2137c34151851b9a40d3509e88f
Yan Gonçalves <yan.paula@americanas.delivery>
2024-06-06 10:01:05 -0300

    Create a customized tenant finder

---
+++ app/Actions/Multitenancy/DomainTenantFinder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Actions\Multitenancy;
+
+use Illuminate\Http\Request;
+use Spatie\Multitenancy\Models\Concerns\UsesTenantModel;
+use Spatie\Multitenancy\Models\Tenant;
+use Spatie\Multitenancy\TenantFinder\TenantFinder;
+
+class DomainTenantFinder extends TenantFinder
+{
+    use UsesTenantModel;
+
+    public function findForRequest(Request $request): ?Tenant
+    {
+        $host = $request->user()?->id;
+
+        return $this->getTenantModel()::whereDomain($host)->first();
+    }
+}
